03741722684e7249eeb17f14d91b386bd963e6ea
Tom Walker <tom.w@niceandserious.com>
2017-11-10 12:11:41 +0000

    first commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
 $(document).ready(function() {
-  console.log("boom")
+
+  var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+	var renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+  camera.position.z = 10;
+
+  var red = new THREE.Color(0xFFFFFF);
+  var green = new THREE.Color(0x00FF00);
+
+  var material1 = new THREE.MeshBasicMaterial({color: red});
+  var material2 = new THREE.MeshBasicMaterial({color: green});
+
+  var loader = new THREE.FontLoader();
+
+  loader.load('./fonts/testFont8.json', function ( font ) {
+    console.log(font)
+  	var textGeom = new THREE.TextGeometry('Nice', {
+  		font: font,
+  		size: 10,
+  		height: 10,
+  		curveSegments: 10
+  	});
+
+    textGeom.normalize();
+
+    var textMesh = new THREE.Mesh(textGeom, [material1, material2]);
+
+    scene.add(textMesh)
+  });
+
+	var animate = function () {
+		requestAnimationFrame( animate );
+		renderer.render(scene, camera);
+	};
+
+	animate();
+
 });
